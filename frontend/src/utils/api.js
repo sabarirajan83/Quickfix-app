31eb2c1798666f83d475830aa99fda966111d416
sabarirajan83 <sabarirajan450@gmail.com>
2026-03-01 23:40:04 +0530

    Fix GitHub Action to target backend folder

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://quickfix-eshnafcudrbdfch8.eastasia-01.azurewebsites.net",
 });
 
 api.interceptors.request.use((config) => {
